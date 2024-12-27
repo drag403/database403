b46e0fb81237e05f2b5424d0fb7d9de9dd9879c1
liang be <zajazis50@tiboj.com>
2024-12-28 04:27:10 +0700

    Update index.php

--- database403/index.php
+++ database403/index.php
@@ -1,1 +1,18 @@
+    <?php
+    // Periksa apakah request method adalah POST
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        // Ambil data JSON dari body request
+        $json = file_get_contents('php://input');
+        $data = json_decode($json, true);
 
+        // Simpan data ke file log atau tampilkan
+        file_put_contents('notifications.log', $json . PHP_EOL, FILE_APPEND);
+
+        // Tampilkan data untuk debugging
+        echo '<pre>';
+        print_r($data);
+        echo '</pre>';
+    } else {
+        echo 'No data received.';
+    }
+    ?>
